002360560b3458a3a7372ececd18962a4089bba6
sudeeprss83 <sudeep.rss83@gmail.com>
2021-05-01 11:48:12 +0530

    frontend

---
+++ js/contact-us.js
@@ -0,0 +1,58 @@
+let typeValue = "feedback";
+const send = document.getElementById("send");
+const type = document.getElementById("dropdown");
+const source = document.getElementById("alert-user");
+const queries = document.getElementById("queries");
+
+queries.addEventListener("click", (e) => {
+  location.href = "http://localhost:5500/frontend/feedback.html";
+});
+
+type.addEventListener("change", (e) => {
+  typeValue = type.value;
+});
+
+send.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  if (name.length == 0 || email.length == 0 || message.length == 0) {
+    source.innerHTML = `<div class="alert alert-warning" role="alert">
+                              Please fill the form !
+                          </div>`;
+    setTimeout(() => {
+      source.innerHTML = ``;
+    }, 2000);
+    return
+  }
+  const url = "http://localhost:3000/feedbacks";
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      type: typeValue,
+      message: message,
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then(() => {
+      source.innerHTML = `<div class="alert alert-success" role="alert">
+                              Your response have been saved !
+                          </div>`;
+      setTimeout(() => {
+        source.innerHTML = ``;
+      }, 2000);
+    })
+    .catch((err) => {
+      console.log(err);
+      source.innerHTML = `<div class="alert alert-warning" role="alert">
+                              Oops Something went wrong :( !
+                          </div>`;
+      setTimeout(() => {
+        source.innerHTML = ``;
+      }, 2000);
+    });
+});
---
+++ js/feedback.js
@@ -0,0 +1,25 @@
+const tableBody = document.getElementById("contact-us-data");
+const back = document.getElementById("back");
+
+back.addEventListener("click", (e) => {
+  location.href = "http://localhost:5500/frontend/contact-us.html";
+});
+
+const url = "http://localhost:3000/feedbacks";
+fetch(url, {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+})
+  .then((response) => response.json())
+  .then((json) => {
+    json.data.forEach((item, i) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<th scope="row">${i+1}</th>
+                        <td>${item.name}</td>
+                        <td>${item.email}</td>
+                        <td>${item.type}</td>
+                        <td>${item.message}</td>`;
+      tableBody.appendChild(tr);
+    });
+  })
+  .catch((err) => console.log(err));
